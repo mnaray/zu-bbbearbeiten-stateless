70c0cf52df799ba4b51b4e9bbe245f40cc3fb9de
mnaray <m.naray.inf21@stud.bbbaden.ch>
2023-08-18 17:02:46 +0200

    format python files

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 import helper
 from flask import Flask, request, Response, render_template, redirect, url_for
+
 app = Flask(__name__)
 
 # @app.route("{route}") bestimmt was passiert wenn die jeweilige Route in die Suchleiste eingegeben wird. (Routing)
 
+
 @app.route("/")
 def index():
     items = helper.get_all()
-    return render_template('index.html', items=items) # Hier wird das index.html mit den Daten in items gerendert.
+    return render_template(
+        "index.html", items=items
+    )  # Hier wird das index.html mit den Daten in items gerendert.
 
 
-@app.route('/add', methods=["POST"])
+@app.route("/add", methods=["POST"])
 def add():
     text = request.form.get("text")
     helper.add(text)
-    return redirect(url_for("index")) # Hier wird index() mit der überarbeiteten Liste neu geladen.
+    return redirect(
+        url_for("index")
+    )  # Hier wird index() mit der überarbeiteten Liste neu geladen.
 
 
-@app.route('/update/<int:index>')
+@app.route("/update/<int:index>")
 def update(index):
     helper.update(index)
-    return redirect(url_for("index")) # Hier wird auch index() mit der überarbeiteten Liste neu geladen.
-
+    return redirect(
+        url_for("index")
+    )  # Hier wird auch index() mit der überarbeiteten Liste neu geladen.
