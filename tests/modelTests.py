f02b42e3698eec6d96ab061e5a8b4855bc2ddb6b
mnaray <m.naray.inf21@stud.bbbaden.ch>
2023-08-25 16:25:20 +0200

    add test for helper.update

--- tests/modelTests.py
+++ tests/modelTests.py
@@ -11,5 +11,18 @@ class TestModelMethods(unittest.TestCase):
         expected = helper.Todo("test todo")
         # act
         helper.add(expected.title)
+        actual = helper.get(0)
         # assert
-        self.assertEqual(expected, helper.get_all()[0])
+        self.assertEqual(expected, actual)
+
+    def test_update(self):
+        # arrange
+        todo = helper.Todo("test todo")
+        helper.add(todo.title)
+        expected = helper.Todo("test todo")
+        expected.isCompleted = True
+        # act
+        helper.update(0)
+        actual = helper.get(0)
+        # assert
+        self.assertEqual(expected, actual)
