0fbc1767860e05244ecf78cd9a72d6c311d5517d
mnaray <m.naray.inf21@stud.bbbaden.ch>
2023-09-01 16:26:47 +0200

    implement due dates, closes #10

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ def index():
 @app.route("/add", methods=["POST"])
 def add():
     text = request.form.get("text")
-    helper.add(text)
+    date = request.form.get("date")
+    helper.add(text, date)
     return redirect(
         url_for("index")
     )  # Hier wird index() mit der überarbeiteten Liste neu geladen.
