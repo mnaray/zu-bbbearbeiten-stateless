76cbe26d133a4066286b5dc4922604f6dd915cc3
Nicola Colic <Nicola.Colic@bbbaden.ch>
2023-09-02 16:59:43 +0200

    try

---
+++ helper.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass
+import datetime
+import operator
+
+items = []
+
+
+@dataclass
+class Item:
+    text: str
+    date: datetime
+    isCompleted: bool = False
+
+
+def oneWeekFromToday():
+    today = datetime.datetime.now()
+    oneWeek = datetime.timedelta(weeks=1)
+    return today + oneWeek
+
+
+def add(text, date=None):
+    text = text.replace('b', 'bbb').replace('B', 'Bbb')
+
+    if date is None:
+        date = oneWeekFromToday()
+    else:
+        date = datetime.datetime.strptime(date, '%Y-%m-%d')
+    items.append(Item(text, date))
+    items.sort(key=operator.attrgetter("date"))
+
+
+def get_all():
+    return items
+
+
+def get(index):
+    return items[index]
+
+
+def update(index):
+    items[index].isCompleted = not items[index].isCompleted
