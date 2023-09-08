0da96ad579bdbd80cabd3e346bbcaeb3d9b6dbcc
mnaray <m.naray.inf21@stud.bbbaden.ch>
2023-09-08 16:59:52 +0200

    resolve issue #15, categories

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -37,3 +37,21 @@ def test_sort(setUp):
     # Then: They should be sorted by date
     for i in range(len(helper.todos) - 1):
         assert helper.todos[i].date < helper.todos[i + 1].date
+
+
+def test_cat(setUp):
+    todos = [
+        ("Universum debuggen", "2023-09-06", helper.categories[1]),
+        ("Superheld werden", "2023-10-25", helper.categories[3]),
+        ("Netto null", "2050-01-01"),
+    ]
+
+    for todo in todos:
+        if len(todo) == 3:
+            helper.add(todo[0], todo[1], todo[2])
+        elif len(todo) == 2:
+            helper.add(todo[0], todo[1])
+
+    assert helper.todos[0].category.name == "School"
+    assert helper.todos[1].category.name == "Chores"
+    assert helper.todos[2].category.name == "Overall"
